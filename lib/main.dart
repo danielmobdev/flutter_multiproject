0c3e4fbc07917ffa1f06ae450404f12eb5151e23
danielmobdev <danielmobdev@gmail.com>
2025-01-01 19:40:25 +0530

    Project added

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Multi-Tenant App',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello Multi-Tenant')),
+        body: Center(child: Text('Hello from Flutter!')),
+      ),
+    );
+  }
+}
